07617973d02281e4d0aab03e391a70823680c4fd
Spanfile <spansdev@gmail.com>
2020-08-11 02:21:09 +0300

    Update sync-factorio.mods.sh

--- scripts/sync-factorio-mods.sh
+++ scripts/sync-factorio-mods.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avh $HOME/.factorio/mods/ factorio.int.spans.me:/opt/factorio/mods/ --delete --exclude sync.sh --exclude .vscode/ --exclude test-mod/ --exclude debugadapter* --exclude mod-list.json --exclude mod-settings.dat
+rsync -avh $HOME/.factorio/mods/ factorio.int.spans.me:/opt/factorio/mods/ --delete --exclude "debugadapter*" --include "*.zip" --exclude "*"
