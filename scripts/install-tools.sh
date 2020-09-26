d3bf33f2d770bda96421c8614a27a2e3415d06b4
Spanfile <spansdev@gmail.com>
2020-09-26 15:58:06 +0300

    Add cargo-audit to install-tools.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,6 +1,6 @@
 echo -e "\nInstalling Rust tools...\n"
 
-for p in cache expand tree udeps update watch tarpaulin count bloat
+for p in cache expand tree udeps update watch tarpaulin count bloat audit
 do
 	cargo install cargo-$p
 done
