b5c2b6d39311faa77aa43c03672e0c355e86068f
Spanfile <spansdev@gmail.com>
2021-02-09 15:44:31 +0200

    Remove scripts from dotfiles

--- scripts/sync-factorio-mods.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avh $HOME/.factorio/mods/ factorio.int.spans.me:~/factorio/mods/ --delete --exclude "debugadapter*" --include "*.zip" --include "mod-settings.dat" --include "mod-list.json" --exclude "*"
